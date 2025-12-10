7f0a4866eb5fbf9336aaacbbd6f8fd038dec71da
r4ilsonLeite <railsonleite@gmail.com>
2025-12-09 21:14:49 -0300

    feat: classe usuario

---
+++ user.py
@@ -0,0 +1,20 @@
+class User:
+    def __init__(self, name, adress, phone, email, date_register, password, status ):
+        self.name = name
+        self.adress = adress
+        self.phone = phone
+        self.email = email
+        self.date_register = date_register
+        self.password = password
+        self.status = status
+    def check_password(self, password: str) -> bool:
+        return self.password == password
+    def to_dict(self)-> dict: 
+        return{
+            'name': self.name
+        }
+if __name__ == '__main__':
+    user = User ( 'joao', 'rua' ,'joao', 'joao@joao.com', '09/12/25', 'senha', 'ativo' )
+    print(user.to_dict())
+    print(user.check_password('semsenha'))
+    print(user.check_password('senha'))
