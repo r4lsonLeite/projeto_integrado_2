981987e36a2fff9c6a940bab4a6d9770c9c466af
r4ilsonLeite <railsonleite@gmail.com>
2025-12-11 19:21:38 -0300

    classe progressoconteudo

---
+++ progressoconteudo.py
@@ -0,0 +1,10 @@
+class Progconteudo():
+    def __init__(prog, user, conteudo, status, ultimoacesso, percentual):
+        self.prog = prog
+        self.user = user
+        self.conteudo = conteudo    
+        self.status = status
+        self.ultimoacesso = ultimoacesso
+        self.percentual = percentual
+        
+        pass
