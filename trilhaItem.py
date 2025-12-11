39d1aa383e4f37018cd5f8a1e45ac255c70a2bdb
r4ilsonLeite <railsonleite@gmail.com>
2025-12-11 19:14:58 -0300

    criação classe trilhagem

---
+++ trilhaItem.py
@@ -0,0 +1,9 @@
+class Trilhaitem():
+    def __init__(trilhaitem, tilha, conteudo, ordem, obrigatorio):
+        self.trilhaitem = trilhaitem
+        self.trilha = trilha
+        self.conteudo = conteudo
+        self.ordem = ordem
+        self.obrigatorio = obrigatorio
+        
+        pass
