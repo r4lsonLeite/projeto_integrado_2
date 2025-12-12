6387184687ba6d0cae186bc1f4ba4163e5c44aa6
Marcos Felipe Ferreira Duarte <marcosfe54321@gmail.com>
2025-12-11 22:03:07 -0300

    payment.py

--- payment.py
+++ payment.py
@@ -1,5 +1,6 @@
 class Payment:
-    def __init__(self, payment_id, invoice_id, amount, gateway_type, transaction_gateway, status, payment_date):
+    def __init__(self, payment_id, invoice_id, amount, 
+                 gateway_type, transaction_gateway, status, payment_date):
         self.payment_id = payment_id
         self.invoice_id = invoice_id
         self.amount = amount
@@ -7,16 +8,23 @@ class Payment:
         self.transaction_gateway = transaction_gateway
         self.status = status
         self.payment_date = payment_date
-    def process_and_confirm(self, confirmation_date):
+    def to_dict(self):
+        return {
+            'payment_id': self.payment_id,
+            'invoice_id': self.invoice_id,
+            'amount': self.amount,
+            'gateway_type': self.gateway_type,
+            'transaction_gateway': self.transaction_gateway,
+            'status': self.status,
+            'payment_date': self.payment_date
+        }
+    def process_and_confirm(self, confirmation_date, invoice_object):
         if self.status == 'pending':
             print(f"Payment {self.payment_id}: Processing via {self.gateway_type}...")
             self.status = 'confirmed'
             self.payment_date = confirmation_date
+            invoice_object.register_payment_confirmed()
             print("Payment confirmed and registered.")
             return True
-            print(f"Payment {self.payment_id} could not be processed. Status: {self.status}.")
+        print(f"Payment {self.payment_id} could not be processed. Status: {self.status}.")
         return False
-
-
-
-    
