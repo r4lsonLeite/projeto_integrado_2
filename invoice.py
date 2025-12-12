b8259b4027fe20db381de5fc35013c38899ffe1f
Marcos Felipe Ferreira Duarte <marcosfe54321@gmail.com>
2025-12-11 21:59:02 -0300

    invoice.py

--- invoice.py
+++ invoice.py
@@ -1,4 +1,5 @@
 class Invoice:
+    
     def __init__(self, invoice_id, order_id, amount, due_date, client_id, status, issue_date):
         self.invoice_id = invoice_id
         self.order_id = order_id
@@ -6,7 +7,17 @@ class Invoice:
         self.due_date = due_date
         self.client_id = client_id
         self.status = status
-        self.issue_date = issue_date # Atribuído diretamente
+        self.issue_date = issue_date
+    def to_dict(self):
+        return {
+            'invoice_id': self.invoice_id,
+            'order_id': self.order_id,
+            'amount': self.amount,
+            'due_date': self.due_date,
+            'client_id': self.client_id,
+            'status': self.status,
+            'issue_date': self.issue_date
+        }
     def generate_charge(self):
         if self.status == 'pending':
             self.status = 'awaiting_payment'
