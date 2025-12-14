f8d9316a8de3bc4e337494d2979c88db66a68519
r4ilsonLeite <railsonleite@gmail.com>
2025-12-14 10:00:28 -0300

    correção na classe conteudo

--- conteudo.py
+++ conteudo.py
@@ -1,9 +1,24 @@
 class Conteudo():
-    def __init__(titulo, descrição, categoria, url, carga_horaria, visibilidade):
+    
+    def __init__(self, titulo, descricao, categoria, url, carga_horaria, visibilidade):
         self.titulo = titulo 
-        self.descrição = descrição 
+        self.descrição = descricao 
         self.categoria = categoria
         self.url = url 
         self.carga_horaria = carga_horaria
+        self.visibilidade = visibilidade
         
-        pass
+        
+        # Criando uma instância (um objeto) da classe Conteudo
+meu_curso = Conteudo(
+    titulo="Fundamentos",
+    descricao="Curso introdutório",
+    categoria="vendas",
+    url="https://www.google.com/",
+    carga_horaria=40,
+    visibilidade="Publico"
+)
+
+# Acessando os dados
+print(f"Curso: {meu_curso.titulo}")
+print(f"Carga Horária: {meu_curso.carga_horaria} horas")
