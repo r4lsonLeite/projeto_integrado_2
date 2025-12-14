308098b3c7baae05529f7625ff9415481943a92a
r4ilsonLeite <railsonleite@gmail.com>
2025-12-14 10:02:06 -0300

    correção na classe trilhagem

--- trilhaItem.py
+++ trilhaItem.py
@@ -1,9 +1,8 @@
 class Trilhaitem():
-    def __init__(trilhaitem, tilha, conteudo, ordem, obrigatorio):
+    def __init__(self, trilhaitem, trilha, conteudo, ordem, obrigatorio):
         self.trilhaitem = trilhaitem
         self.trilha = trilha
         self.conteudo = conteudo
         self.ordem = ordem
         self.obrigatorio = obrigatorio
         
-        pass
